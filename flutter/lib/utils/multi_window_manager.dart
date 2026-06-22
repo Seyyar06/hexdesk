658d430863059ec8f2d767e84e7072c4c6b7c6e5
Seyyar06 <nuri.66.nk@gmail.com>
2026-06-22 16:32:02 +0300

    Fix undefined bind compiler error in multi_window_manager.dart

--- flutter/lib/utils/multi_window_manager.dart
+++ flutter/lib/utils/multi_window_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:desktop_multi_window/desktop_multi_window.dart';
+import 'package:flutter_hbb/models/platform_model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
